1ddd3e4202985fdbff37d82cfe56ceccb7801624
HiDoYa <hiroyagojo@gmail.com>
2017-05-07 23:41:44 -0700

    Adding for loop

---
+++ first-program/test.py
@@ -0,0 +1,19 @@
+print("Hello world")
+
+x = 10;
+
+#if else
+if x == 10:
+    print("X is 10")
+else:
+    print("""x is not 10""")
+
+#while loop
+while x < 15:
+    print(x)
+    x = x + 1
+
+#for loop and sequence object
+listOfSomething = ["something", "something else", "this is really something", "lorem ipsum dolor"]
+for x in listOfSomething:
+    print(x)
